30b5e3292e8a10644879cf27db2af7eca0c59d84
Igor Ulian <igorulian@outlook.com>
2021-08-13 14:55:53 -0300

    add random answer to the 'read' system

--- src/requests/read-answer.ts
+++ src/requests/read-answer.ts
@@ -2,15 +2,17 @@ import { Timeout } from "../tools/timeout";
 import { GuessAnswer } from "./guess-answer";
 import { IQuestion } from "./questions";
 
-const delay = 1000 //ms
-
-//1628876364
-//9999999999
+const delay = 1000 
 
 export async function ReadAnswer(question:IQuestion,groupid:number){
     console.log('                   📜"Lendo" as parada')
 
     await Timeout(delay)
+    const answer = Random(0,9999999999)
+    await GuessAnswer(question, groupid, answer.toString())
+}
 
-    await GuessAnswer(question, groupid, 'a')
+
+function Random(min:number, max:number){
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
