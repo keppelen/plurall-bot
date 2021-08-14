ab488442662983642e75cef22a1311a30d9816cc
Igor Ulian <igorulian@outlook.com>
2021-08-14 20:23:59 -0300

    add request to save the answer and fixed html parser

--- bot-cli/src/data/save-answer.ts
+++ bot-cli/src/data/save-answer.ts
@@ -1,6 +1,9 @@
 import { IBook } from "../requests/books"
 import { IQuestion } from "../requests/questions"
 import { convert } from 'html-to-text'
+import axios from "axios"
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 export function SaveAnswer(question:IQuestion, groupid:number, answer:string, book:IBook){
@@ -11,15 +14,22 @@ export function SaveAnswer(question:IQuestion, groupid:number, answer:string, bo
     console.log(`                   💾   Question:  ${question.id}`)
     console.log(`                   💾 Resposta: ${answer}`)
 
+    let finalAnswer = answer
+
     if(question.task_type === 'open_response'){
         const html = answer;
         const text = convert(html, {
           wordwrap: 130
         });
-        console.log(`                   💾 Resposta Corrigida: ${text}`)
-        answer = html.replace('-­-­', '-')
-        return
+        finalAnswer = text.replace('-­-­', '-')
+        console.log(`                   💾 Resposta Corrigida: ${finalAnswer}`)
     }
+
+    const body = {
+      answer: finalAnswer
+    }
+
+    axios.post(`${process.env.BACKEND_CONNECTION_IP}/add/${book.id}/${groupid}/${question.id}`, body)
 
     // returns A - B - C... 
 }
