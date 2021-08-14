560fe345d9a3bfe840f6566669672b7402a44e45
Igor Ulian <igorulian@outlook.com>
2021-08-14 17:50:14 -0300

    fixed book pass

--- bot-cli/src/answer-type/guess-multiple-choice.ts
+++ bot-cli/src/answer-type/guess-multiple-choice.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { IBook } from "../requests/books";
 import { GuessAnswer } from "../requests/guess-answer";
 import { IQuestion } from "../requests/questions";
-import { Timeout } from "../tools/timeout";
 
 export async function GuessMultipleChoice(question:IQuestion, groupid:number, book:IBook){
     let alternatives:string[] = ['a','b','c','d','e']
@@ -9,7 +9,7 @@ export async function GuessMultipleChoice(question:IQuestion, groupid:number, bo
     for(let x = 0; x < 3; x++){
         const guess = alternatives[Random(0,alternatives.length-1)]
 
-        const isCorrect = await GuessAnswer(question, groupid, guess)
+        const isCorrect = await GuessAnswer(question, groupid, guess, book)
         if(isCorrect) return
 
         alternatives = alternatives.filter(item => item !== guess)
--- bot-cli/src/data/save-answer.ts
+++ bot-cli/src/data/save-answer.ts
@@ -5,7 +5,10 @@ import { IQuestion } from "../requests/questions"
 
 export function SaveAnswer(question:IQuestion, groupid:number, answer:string, book:IBook){
     console.log(`                   💾 Salvando alternativa correta...`)
-    console.log(`                   💾 Dados:  ${question.id} | ${groupid}`)
+    console.log(`                   💾 Dados:`)
+    console.log(`                   💾   Book:  ${book.id}`)
+    console.log(`                   💾   Group:  ${groupid}`)
+    console.log(`                   💾   Question:  ${question.id}`)
     console.log(`                   💾 Resposta: ${answer}`)
 
     if(question.task_type === 'open_response'){
