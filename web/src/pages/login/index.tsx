aa7949b3186d9286db0a96d9a625b5482eae5e81
Igor Ulian <igorulian@outlook.com>
2021-08-19 12:48:47 -0300

    add loading to login page

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,18 +1,30 @@
 import React from "react"
+import { useState } from "react"
 import { Input, LoginButton, LoginContainer, LoginDescription, LoginText, Page } from "./styles"
+import ReactLoading from 'react-loading';
 
 const Login:React.FC = () => {
+    const [loading,setLoding] = useState(false)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function login(){
+        setLoding(true)
+    }
+
     return (
         <Page>
             <LoginContainer>
                 <LoginText> Login </LoginText>
                 <LoginDescription> Entre com sua conta do plurall para começar a ilegalidade </LoginDescription>
                 
-                <Input placeholder='Digite seu email'/>
-                <Input placeholder='Digite sua senha'/>
+                <Input placeholder='Digite seu email' onChange={v => setEmail(v.target.value)}/>
+                <Input placeholder='Digite sua senha' onChange={v => setPassword(v.target.value)}/>
                 
-                <LoginButton>
-                    Acessar 
+                <LoginButton onClick={() => login()}>
+                    {!loading ? 'Acessar' : 
+                        <ReactLoading type={'spin'} color={'#fff'} height={'20px'} width={'20px'} />
+                    } 
                 </LoginButton>
             </LoginContainer>
         </Page>
