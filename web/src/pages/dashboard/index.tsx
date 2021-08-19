0a469caddf7d50b7df17898b16d4064c456b2b01
Igor Ulian <igorulian@outlook.com>
2021-08-19 14:11:03 -0300

    add dashboard base style

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from "react"
-import { Header, Page } from "./styles"
+import { IconContext } from "react-icons";
+import { BookContent, Content, ContentHeader, ContentTitle, Description, Header, HeaderContent, HeaderTitle, Page, UserData, UserTitle } from "./styles"
+import { FaUserAlt } from 'react-icons/fa';
 
 const Dashboard:React.FC = () => {
     return (
         <Page>
-            <Header> Selecione a apostila que deseja fazer </Header>
+            <Header> 
+                 <HeaderContent> 
+                    <HeaderTitle> Selecione a apostila que deseja fazer </HeaderTitle> 
+                 </HeaderContent>
+            </Header>
+
+            <Content>
+                <ContentHeader>
+                    <Description> Aula dada, aula feita de maneira mais eficiente :D </Description>
+                    <ContentTitle> Todos os cadernos </ContentTitle>
+                </ContentHeader>
+
+                <BookContent>
+                    <div style={{display:'flex'}}>
+                        <IconContext.Provider value={{ color: "#847FBC", size: '15'}}> <FaUserAlt/> </IconContext.Provider>
+                        <UserTitle> Usuário logado:  </UserTitle>
+                        <UserData>  teste@teste.com - dadadadasd</UserData>
+                    </div>
+
+
+                </BookContent>
+
+            </Content>
             
         </Page>
     )
--- web/src/pages/dashboard/styles.tsx
+++ web/src/pages/dashboard/styles.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Page = styled.div`
     display: flex;
+    flex-direction: column;
     margin: 0;
     padding: 0;
     background-color: #fff;
@@ -13,18 +14,86 @@ export const Page = styled.div`
 `
 
 export const Header = styled.header`
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&display=swap');  
-    font-family: 'Poppins', sans-serif;
     display: flex;
     background-color: #fff;
     width: 100%;
-    height: 70px;
-    position: fixed;
+    height: 64px;
     top: 0;
     box-shadow: rgba(38, 41, 48, 0.1) 0px 2px 0px 0px;
     align-items: center;
     justify-content: center;
-    color: #c099ff;
     font-size: 34px;
 `
+export const HeaderContent = styled.div`
+    display: flex;
+    width: 60%;
+    height: 60%;
+    align-items: center;
+`
 
+export const HeaderTitle = styled.p`    
+    @import url('https://fonts.googleapis.com/css2?family=Spartan&display=swap');
+    font-family: 'Spartan', sans-serif;
+    color: #c099ff;  //c099ff 
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 60%;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ContentHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 85px;
+    justify-content: flex-start;
+`
+
+export const ContentTitle = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap');
+    font-family: 'Nunito', sans-serif;
+    font-size: 25px;
+    letter-spacing: -0.3px;
+    color: rgb(101, 92, 163);
+`
+
+export const Description = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Spartan:wght@200&display=swap');
+    font-family: 'Spartan', sans-serif;
+    color: #4a4a4a;
+    font-size: 13px;    
+    font-weight: lighter;
+    letter-spacing: -0.3px;
+    margin-top: 45px;
+`
+
+export const BookContent = styled.div`
+    display: flex;
+    margin-top: 65px;
+    width: 96%;
+    height: 100%;
+`
+
+export const UserTitle = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+    font-family: 'Open Sans', sans-serif;
+    color: #4a4a4a;
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 24px;
+    margin-left: 7px;
+`
+
+export const UserData = styled.p`
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@100&display=swap');
+    font-family: 'Open Sans', sans-serif;
+    color: #4a4a4a;
+    font-size: 13px;
+    line-height: 24px;
+    margin-left: 10px;
+`
