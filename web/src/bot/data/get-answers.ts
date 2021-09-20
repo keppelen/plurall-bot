8fdad8f101da07810a9b9f48cc2185434ecf1491
Igor Ulian <igorulian@outlook.com>
2021-09-20 13:46:49 -0300

    remove answers log

--- web/src/bot/data/get-answers.ts
+++ web/src/bot/data/get-answers.ts
@@ -12,11 +12,11 @@ interface IAnswer{
 export let answers:IAnswer[]|null = null
 
 export async function GetAnswers(bookid:string){
-    console.log('Pegando as repostas ta apostila...')
+    // console.log('Pegando as repostas ta apostila...')
     try{
         const response = await api.get(`/answer/list/${bookid}`, authorizaton)
         answers = response.data
-        console.log(answers)
+        // console.log(answers)
     }catch{
         console.log('Ocorreu um erro ao buscar as respostas :/')
     }
